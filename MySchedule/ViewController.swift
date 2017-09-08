7fd9910ec03be234a369bcdb440ef50296c67466
Jan-Mark Dannenberg <jan-markdannenberg@MacBook-Pro.local>
2017-09-08 13:21:29 +0200

    Initial Commit

---
+++ MySchedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySchedule
+//
+//  Created by Jan-Mark Dannenberg on 08/09/2017.
+//  Copyright © 2017 Jan-Mark Dannenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
