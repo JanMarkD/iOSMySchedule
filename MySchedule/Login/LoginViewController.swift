e9958a9406eed189519a05ae416663b54c3eb88b
Marjolein Uitbeijerse <marjoleinuitbeijerse@mbpvanmarjolein.home>
2017-10-17 14:49:56 +0200

    .

--- MySchedule/Login/LoginViewController.swift
+++ MySchedule/Login/LoginViewController.swift
@@ -77,12 +77,12 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let defaults = UserDefaults.standard
-        let login = defaults.value(forKey: "Login") as! [String]
-        let username = login[0]
-        let password = login[1]
-        
-        self.passwordTextField.text = password
-        self.usernameTextField.text = username
+//        let login = defaults.value(forKey: "Login") as! [String]
+//        let username = login[0]
+//        let password = login[1]
+//        
+//        self.passwordTextField.text = password
+//        self.usernameTextField.text = username
         
         
         // Do any additional setup after loading the view, typically from a nib.ddd
